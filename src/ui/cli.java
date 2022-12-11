d627b09286e9c9e1a6e248339d06d087a7acb349
francescoditria <78537500+francescoditria@users.noreply.github.com>
2022-12-11 14:12:02 +0100

    Add files via upload

--- src/ui/cli.java
+++ src/ui/cli.java
@@ -24,11 +24,16 @@ public class cli {
 	
 	private void showHelp()
 	{
-		System.out.println("\nCommands");
-		System.out.println("-generate <min>,<max>,<size>");
-		System.out.println("-model <filename>");
-		System.out.println("-change <buckets>");
-		System.out.println("-query <min>,<max>\n");
+		System.out.println("\nCommands:");
+		System.out.println("- generate <min>,<max>,<size>");
+		System.out.println("\tgenerates a file with random <size> data in range [min,max] (testing scope only)");
+		System.out.println("- model <filename>");
+		System.out.println("\tbuilds a histogram model using file <filename>");
+		System.out.println("- change <#buckets>");
+		System.out.println("\tmanually changes the number of buckets in the generated histogram model");
+		System.out.println("- query <min>,<max>");
+		System.out.println("\tapproximate query processing in range [min,max]\n");
+
 		
 		
 		
@@ -42,7 +47,7 @@ public class cli {
 	
 	public void showIntro()
 	{
-		System.out.println("Starting jhisto");
+		System.out.println("Starting histmo");
 		System.out.println("Type <bye> to quit");
 		
 	}
